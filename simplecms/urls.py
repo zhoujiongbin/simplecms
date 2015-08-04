ccd4601862bd65c42327a88583f379898c8491c4
GUIYU <guiyu@mengjukeji.com>
2015-08-04 17:28:32 +0800

    增加了模板目录设置，增加了common文件夹，和实现了后台用户和公司详情的部分功能

--- simplecms/urls.py
+++ simplecms/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+
+from admin.user import views
+from common.views import error
 # from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^error/$',error),
     url(r'^/|^index/|^home/', include('home.urls')),
-    url(r'^article/', include('admin.article.urls')),
-    url(r'^user/', include('admin.user.urls')),
-    url(r'^cat/', include('admin.cat.urls')),
-    url(r'^detail/', include('admin.detail.urls')),
-    url(r'^$|^test/',include('admin.testcms.urls'))
+    url(r'^admin/article/', include('admin.article.urls')),
+    url(r'^admin/user/', include('admin.user.urls')),
+    url(r'^admin/cat/', include('admin.cat.urls')),
+    url(r'^admin/detail/', include('admin.detail.urls')),
+    url(r'^$|^test/',include('admin.testcms.urls')),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^sign/$', views.sign, name='sign'),
 ]
