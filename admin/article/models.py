3187da15ee0ad3a1bdf7597a9a4c36413eac89fd
wwl <787395955@qq.com>
2015-08-04 18:32:23 +0800

    article

--- admin/article/models.py
+++ admin/article/models.py
@@ -3,8 +3,7 @@ from admin.cat.models import Cat
 
 class Article(models.Model):
     article_title = models.CharField(max_length=200)
-    publish_time = models.DateTimeField('date published')
+    publish_time = models.DateTimeField(auto_now_add=True)
     article_author = models.CharField(max_length=50)
     article_content = models.TextField()
     article_cat_id = models.ForeignKey(Cat)
-
--- simplecms/settings.py
+++ simplecms/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'simplecms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\','/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
