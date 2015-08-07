de2214b50e9a36ede8487060e5b51d0ff832ed27
GUIYU <guiyu@mengjukeji.com>
2015-08-07 14:04:47 +0800

    将django配置到服务器，修改了一下样式

--- simplecms/wsgi.py
+++ simplecms/wsgi.py
@@ -7,26 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplecms.settings")
-#
-# application = get_wsgi_application()
 import os
-import sys
 
-sys.stdout = sys.stderr
+from django.core.wsgi import get_wsgi_application
 
-from os.path import abspath, dirname, join
-from site import addsitedir
-
-from django.core.handlers.wsgi import WSGIHandler
-
-sys.path.insert(0, abspath(join(dirname(__file__), "../")))
-sys.path.insert(0, abspath(join(dirname(__file__), ". . /. . /")))
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "simplecms.settings"
-
-application = WSGIHandler()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplecms.settings")
+application = get_wsgi_application()
